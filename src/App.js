05839a98d60db986ed603ff375fcc74e794d94c8
bobmacneal <bob.macneal@workingcompany.com>
2019-05-23 09:24:39 -0500

    Add material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import 'typeface-roboto'
+import {AppBar, Typography, withStyles} from '@material-ui/core'
+import Logo from './images/logo.png'
+import {MuiThemeProvider} from '@material-ui/core/styles'
+import {PropTypes} from 'prop-types'
+import React from 'react'
+import simpleTheme from './simpleTheme'
 
-function App() {
-  return (
-    <div>
-      <h1>cypress-search-tutorial</h1>
+const App = ({classes}) => (
+  <MuiThemeProvider theme={simpleTheme}>
+    <AppBar color="primary" elevation={1} position="fixed" className={classes.appBar}>
+      <React.Fragment>
+        <div className={classes.logo}>
+          <img src={Logo} height="40" alt="cypress-search-tutorial" />
+        </div>
+        <div className={classes.title}>
+          <Typography
+            align="right"
+            variant="subtitle2"
+            color="black"
+          >
+            {`cypress-search-tutorial v-${process.env.REACT_APP_VERSION}`}
+          </Typography>
+        </div>
+      </React.Fragment>
+    </AppBar>
+    <div style={{margin: '80px 15px 15px 15px'}}>
+      CONTENT
     </div>
-  );
+  </MuiThemeProvider>
+)
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
 }
 
-export default App;
+const globalStyles = {
+  appBar: {
+    alignItems: 'center',
+    display: 'flex',
+    flexWrap: 'wrap',
+    paddingLeft: '15px',
+    paddingRight: '20px',
+    height: '60px',
+    flexDirection: 'row',
+  },
+  logo: {
+    flex: 1,
+  },
+  title: {
+    flex: 1,
+  },
+  '@global': {
+    'html': {
+      backgroundColor: '#fff',
+      fontFamily: '"Roboto", sans-serif',
+    },
+    'body': {
+      backgroundColor: '#fff',
+    },
+  },
+}
+
+export default withStyles(globalStyles)(App)
